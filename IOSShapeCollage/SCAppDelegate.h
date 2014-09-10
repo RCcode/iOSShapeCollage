196e2b519603d8a97764f7523d749761a1b3067d
wsq-wlq <wsq-wlq@wsq-wlqdeMacBook-Pro.local>
2014-09-10 14:06:06 +0800

    Initial Commit

---
+++ IOSShapeCollage/SCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SCAppDelegate.h
+//  IOSShapeCollage
+//
+//  Created by wsq-wlq on 14-9-10.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IOSShapeCollage/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IOSShapeCollage
+//
+//  Created by wsq-wlq on 14-9-10.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SCAppDelegate class]));
+    }
+}
